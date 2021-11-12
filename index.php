1ccfb229bc770a1da20bbb211ee97e3d0a858383
Benaiah Finkel <b.finkel@bluewin.ch>
2021-11-12 16:19:08 +0100

    Read configuration from environment variables

--- index.php
+++ index.php
@@ -5,14 +5,14 @@
 // "http://localhost:8888/comem-archidep-php-todo-exercise/", then BASE_URL
 // should be "/comem-archidep-php-todo-exercise/". If you are accessing the
 // application at "http://localhost:8888", then BASE_URL should be "/".
-define('BASE_URL', '/comem-archidep-benaiah-karen/');
+define('BASE_URL', getenv('TODOLIST_BASE_ULR' ?: '/'));
 
 // Database connection parameters.
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
-define('DB_NAME', 'todolist');
-define('DB_HOST', 'localhost');
-define('DB_PORT', '3306');
+define('DB_USER', getenv('TODOLIST_DB_USER') ?: 'todolist');
+define('DB_PASS', getenv('TODOLIST_DB_PASS'));
+define('DB_NAME', getenv('TODOLIST_DB_HOST') ?: 'todolist');
+define('DB_HOST', getenv('TODOLIST_DB_HOST') ?: '127.0.0.1');
+define('DB_PORT', getenv('TODOLIST_DB_PORT') ?: '3306');
 
 $db = new PDO('mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
 $items = array();
